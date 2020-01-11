760bbd9987cd3451b1e730fbd2b37d84f4148028
derbakias <dimitrios.danousis@gmail.com>
2020-01-12 00:07:06 +0200

    beta version 0.01

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,19 @@
-const key = '&apikey=HBEG3YH190T4G4U4';
 // main variables
 const update = document.querySelector('.update');
 const search = document.querySelector('#search');
 let dateHtml = document.querySelector('.date h5');
 let timeHtml = document.querySelector('.date h2');
+let chartContainer = document.querySelector(".chart-container");
 const boxContainer = document.querySelector('.box-container');
 const boxes = document.querySelectorAll('.box');
 const stockList = ['MSFT', 'AAPL', 'TSLA', 'FB'];
 const link = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='
+const key = '&apikey=HBEG3YH190T4G4U4';
 
 // main event listeners
-// update.addEventListener('click', updateData);
+update.addEventListener('click', makePromises);
 // search.addEventListener('keyup', searchCompany);
-boxContainer.addEventListener('click', (e) => console.log(e.target));
+boxContainer.addEventListener('click', displayData);
 
 // set the time with an interval
 timeHtml.textContent = '';
@@ -31,80 +32,124 @@ function setDateTime(){
   return timeHtml.textContent;
 }
 
-// get the current time
-let currentTime = setDateTime();
-
 // function make requests and get promises
 function makePromises(){
-  Promise.all(stockList.map(item => {
+  Promise.all(stockList.map(stock => {
     // only for test on local files
-    // const src = `json_files/${item}.json`
-    const src = link + item + key;
+    const src = `json_files/${stock}.json`
+    // const src = link + stock + key;
     getData(src)
-      .then(data => setData(data, item))
+      .then(data => setData(data, stock))
       .catch(err => console.log(err));
   }))
 }
 
 // call make promises function
-// makePromises();
+setTimeout(() => makePromises(), 2000);
+
 // setData function and variables
 function setData(data, item){
+// get the current time
+  let currentTime = setDateTime();
   let box = boxes[stockList.indexOf(item)].children[0];
   let symbol = data['Global Quote']['01. symbol'];
-  let price =  data['Global Quote']['05. price'];
+  let price =  parseFloat(data['Global Quote']['05. price']);
   let change = parseFloat(data['Global Quote']['10. change percent'].replace(/%/, ''));
   let template = `
   <p class="stock-name">${symbol}</p>
-  <p class="current-price">${price}</p>
-  <p class="change ${change > 0 ? 'up' : 'down'}"><i class="fas fa-arrow-${change > 0 ? 'up' : 'down'}"></i> ${change}</p>
+  <p class="current-price">${price.toFixed(2)}</p>
+  <p class="change ${change > 0 ? 'up' : 'down'}"><i class="fas fa-arrow-${change > 0 ? 'up' : 'down'}"></i> ${change.toFixed(2)}%</p>
   <p class="updated">Last update at ${currentTime}</p>`
   box.innerHTML = template;
 }
 
-getData('hist_data/MSFT.json')
-  .then(value => createChart(value))
-  .catch(err => console.log(err));
+function displayData(e) {
+  if(e.target.classList.contains('stock-name')){
+    const symbol = e.target.textContent;
+    chartContainer.innerHTML = '<div id="chart"></div>';
+    getHistData(symbol);
+  }
+}
+
+// fetch historical data 
+function getHistData(src) {
+  getData(`hist_data/${src}.json`)
+    .then(value => createChart(value))
+    .catch(err => console.log(err));
+}
 
 // create chart and add the data from api
 function createChart(value) {
-  let dataset = [];
+  let price = [];
+  let volume = [];
+  let dates = [];
     for (const key of Object.keys(value["Time Series (Daily)"])) {
-      let open = value["Time Series (Daily)"][key]["1. open"];
-      let high = value["Time Series (Daily)"][key]["2. high"];
-      let low = value["Time Series (Daily)"][key]["3. low"];
-      let close = value["Time Series (Daily)"][key]["4. close"];
+      let open = parseFloat(value["Time Series (Daily)"][key]["1. open"]);
+      let high = parseFloat(value["Time Series (Daily)"][key]["2. high"]);
+      let low = parseFloat(value["Time Series (Daily)"][key]["3. low"]);
+      let close = parseFloat(value["Time Series (Daily)"][key]["4. close"]);
+      let vol = parseFloat(value["Time Series (Daily)"][key]["5. volume"]);
       let time = new Date(key);
-      series = [time.getTime(), open, high, low, close]
-      dataset.push(series);
+      price.push(close);
+      volume.push(vol);
+      dates.push(time.getTime());
     }
+  // chart options
   let options = {
     series: [{
-      data: ''
+      name: 'Price',
+      type: 'line',
+      data: price
+    }, {
+      name: 'Volume',
+      type: 'bar',
+      data: volume
     }],
     chart: {
-      type: 'candlestick',
-      height: '95%'
+      height: '95%',
+      type: 'line',
+      foreColor: '#984f7A',
+    },
+    plotOptions: {
+      bar: {
+          columnWidth: '20%',
+      },
+    },
+    stroke: {
+      width: [2, 0]
     },
     title: {
-      text: 'CandleStick Chart',
-      align: 'left'
+      text: value["Meta Data"]["2. Symbol"],
+      align: 'left',
     },
+
+    labels: dates
+    ,
     xaxis: {
       type: 'datetime'
     },
-    yaxis: {
-      tooltip: {
-        enabled: true
-      }
-    }
+    yaxis: [{
+      title: {
+        text: 'Price',
+      },
+      decimalsInFloat: 2,
+      min: Math.min.apply(null ,price),
+      max: Math.max.apply(null, price),
+    }, {
+      opposite: true,
+      title: {
+        text: 'Volume'
+      },
+      decimalsInFloat: 0
+    }]
   };
-  options.series['0']['data'] = dataset.slice(0,8);
+
   let chart = new ApexCharts(document.querySelector("#chart"), options);
   chart.render();
-  // console.log(dataset);
+  
+  
+  
 }
 
-
-
-
+// initialise chart
+getHistData('MSFT');
